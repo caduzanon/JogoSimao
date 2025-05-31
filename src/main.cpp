32ea8c110036c83b2ab930f9cb893e4f3a0edad8
caduzanon <carloseduzanon@gmail.com>
2025-05-31 14:28:25 -0300

    cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,31 @@
-#include <SFML/Graphics.hpp>
+#include "graphics.h"
+
 int main(){
 
-  return 0;
+    Graphics gGraphics(1920, 1080, "teste");
+
+    windowHandle gWindowHandle = gGraphics.getWindowHandle();
+    
+    spriteID testSprite = gGraphics.createSprite(gGraphics.loadTexture("../assets/green_alien.png"));//loadTexture retorna o ID da textura direto para createSprite
+    
+    spriteRect testRect(0, 0, 16, 20);
+    gGraphics.setSpriteRect(testSprite, testRect);
+    
+    gGraphics.setSpritePos(testSprite,40.0,10.0);
+    gGraphics.setBackground(testSprite);
+
+    sf::Event testEvent; //a ideia é nao usar tipos da sfml nesse arquivo, mas é so pra testar
+    while(gGraphics.isWindowOpen()){
+        while (gWindowHandle->pollEvent(testEvent))//trata dos eventos, melhor criar uma classe pra isso depois
+        {
+            if (testEvent.type == sf::Event::Closed){
+                gGraphics.closeWindow();
+            }
+        }
+
+        //gGraphics.drawSprite(testSprite);
+        gGraphics.render();
+    }
+    getchar();
+    return 0;
 }
