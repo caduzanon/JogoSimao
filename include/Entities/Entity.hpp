57731ed7ac00ab33bdffbafd300f0255a6502f5e
cadu <carloseduzanon@gmail.com>
2025-05-31 15:58:50 -0300

    Entity and Coordinates Header + CMake adjusts

---
+++ include/Entities/Entity.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <Math/CoordsTL.hpp>
+
+namespace Entities{
+    enum ID {
+        empty = 0,
+        player,
+        platform
+    };
+    class Entity{
+        private:
+            Math::CoordsF position;
+            Math::CoordsF size;
+            ID id;
+        public:
+            Entity(Math::CoordsF position = Math::CoordsF(0.0f, 0.0f), Math::CoordsF size = Math::CoordsF(0.0f, 0.0f), ID id = empty); //construtora inicializando tudo com zero
+            ~Entity();
+            void setPosition(Math::CoordsF position);
+            void setSize(Math::CoordsF size);
+            Math::CoordsF getPosition() const;
+            Math::CoordsF getSize() const;
+            ID getID() const;
+            virtual void render();
+            virtual void update(float dt) = 0;
+            virtual void initialize() = 0;
+    };
+}
