95a24ed602f27d5cc589ec1cc9405ec64ad4fda1
caduzanon <carloseduzanon@gmail.com>
2025-06-02 11:18:09 -0300

    2 players, destrutora de Game

--- src/Game.cpp
+++ src/Game.cpp
@@ -6,16 +6,19 @@ namespace Game{
     using namespace Entities;
     using namespace Characters;
 
-    Unique::Unique() : pGM(GraphicsManager::getInstance()){
+    Unique::Unique() : pGM(GraphicsManager::getInstance()), pPlayer1(nullptr), pPlayer2(nullptr){
         update();
     }
 
     Unique::~Unique(){
         cout << "'Main' destructor called" << endl;
+        delete(pPlayer1);
+        delete(pPlayer2);
     }
 
     void Unique::update(){
-        Player* Player1 = new Player(Vector2f(300.0f, 300.0f), Vector2f(20.0f, 20.0f));
+        pPlayer1 = new Player(Vector2f(300.0f, 300.0f), Vector2f(20.0f, 20.0f), true);
+        pPlayer2 = new Player(Vector2f(400.0f, 400.0f), Vector2f(15.0f, 15.0f), false);
 
         while(pGM->isWindowOpen()){
             Event evento;
@@ -26,8 +29,10 @@ namespace Game{
                 }
             }
             pGM->clearWindow();
-            Player1->move();
-            pGM->render(&Player1->getBody());
+            pPlayer1->move();
+            pPlayer2->move();
+            pGM->render(&pPlayer1->getBody());
+            pGM->render(&pPlayer2->getBody());
             pGM->display();
         }
     }
