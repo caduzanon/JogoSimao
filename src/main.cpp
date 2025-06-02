ef23956ff6e133c0b5104f8bfb601f9a86a3de3e
cadu <carloseduzanon@gmail.com>
2025-06-01 23:46:50 -0300

    todolist

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,12 @@
 #include <Game.hpp>
 
 int main(){
+    //TODO 0106: Segundo Jogador (e suas deps); 
+    //Herança completa da Entidade;
+    //Gravidade e Plataforma;
+    //Lista<T> e ListaEntidades;
+    //Destrutoras!
+    
     Game::Unique MeuJogo;
     return 0;
 }
