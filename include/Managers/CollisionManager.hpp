884c022f1798d00de757f2e210a9a8f324ee51af
cadu <carloseduzanon@gmail.com>
2025-06-16 03:44:13 -0300

    some sort of CM

--- include/Managers/CollisionManager.hpp
+++ include/Managers/CollisionManager.hpp
@@ -9,11 +9,13 @@ namespace Game{
             private:
                 Lists::EntityList* CharactersList;
                 Lists::EntityList* ObstacleList;
+                Lists::EntityList* EnemiesList;
+                Lists::EntityList* ProjectileList;
             public:
                 CollisionManager();
-                CollisionManager(Lists::EntityList* CharactersList, Lists::EntityList* ObstacleList);
+                CollisionManager(Lists::EntityList* CharactersList, Lists::EntityList* ObstacleList, Lists::EntityList* EnemiesList, Lists::EntityList* ProjectileList);
                 ~CollisionManager();
-                void setLists(Lists::EntityList* cList, Lists::EntityList* oList);
+                void setLists(Lists::EntityList* cList, Lists::EntityList* oList, Lists::EntityList* eList, Lists::EntityList* pList);
                 const Vector2f CalculateDistance(Entities::Entity* ent1, Entities::Entity* ent2) const;
                 void update();
         };
--- src/Managers/CollisionManager.cpp
+++ src/Managers/CollisionManager.cpp
@@ -4,21 +4,24 @@ namespace Game::Managers{
     CollisionManager::CollisionManager(){
         ObstacleList = nullptr;
         CharactersList = nullptr;
+        EnemiesList = nullptr;
+        ProjectileList = nullptr;
     }
 
-    CollisionManager::CollisionManager(Lists::EntityList* CharactersList, Lists::EntityList* ObstacleList) : 
+    CollisionManager::CollisionManager(Lists::EntityList* CharactersList, Lists::EntityList* ObstacleList, Lists::EntityList* EnemiesList, Lists::EntityList* ProjectileList) : 
     ObstacleList(ObstacleList), 
-    CharactersList(CharactersList) {
+    CharactersList(CharactersList),
+    EnemiesList(EnemiesList),
+    ProjectileList(ProjectileList)
+    {
 
     }
 
-    void CollisionManager::setLists(Lists::EntityList* cList, Lists::EntityList* oList){
-        if (CharactersList == nullptr){
-            CharactersList = cList;
-        }
-        if (ObstacleList == nullptr){
-            ObstacleList = oList;
-        }
+    void CollisionManager::setLists(Lists::EntityList* cList, Lists::EntityList* oList, Lists::EntityList* eList, Lists::EntityList* pList){
+        if (CharactersList == nullptr)  { CharactersList = cList; }
+        if (ObstacleList == nullptr)    { ObstacleList = oList; }
+        if (EnemiesList == nullptr)     { EnemiesList = eList; }
+        if (ProjectileList == nullptr)  { ProjectileList = pList; }
     }
 
     CollisionManager::~CollisionManager(){
@@ -40,16 +43,31 @@ namespace Game::Managers{
     }
 
     void CollisionManager::update(){
-        //Players + Players, Players & Obst.
-        for (int i=0; i < CharactersList->getSize(); i++){
+
+        //Players + Players
+        for (int i=0; i < CharactersList->getSize() - 1; i++){
             Entities::Entity* aux1 = CharactersList->operator[](i);
-            for(int j=i+1; j< ObstacleList->getSize(); j++){
-                Entities::Entity* aux2 = ObstacleList->operator[](j);
+            for(int j=i+1; j < CharactersList->getSize(); j++){
+                Entities::Entity* aux2 = CharactersList->operator[](j);
                 Vector2f dist = CalculateDistance(aux1, aux2);
                 if (dist.x < 0.0f && dist.y < 0.0f){
-                    //help
+                    aux1->collide(aux2, dist);
                 }
             }
         }
+
+        //Players & Obst.
+        for (int i=0; i < CharactersList->getSize() - 1; i++){
+            Entities::Entity* aux1 = CharactersList->operator[](i);
+            for(int j=i+1; j < ObstacleList->getSize(); j++){
+                Entities::Entity* aux2 = ObstacleList->operator[](j);
+                Vector2f dist = CalculateDistance(aux1, aux2);
+                if (dist.x < 0.0f && dist.y < 0.0f){
+                    aux2->collide(aux1, dist);
+                }
+            }
+        }
+
+        //obstacles don't collide
     }
 }
